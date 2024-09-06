14d2d23ec58786a3b2a87f59ce6d0e43736037b0
Tatiana Fern <112017942+tatianabfern@users.noreply.github.com>
2024-09-06 09:29:35 -0500

    Added SimpleCov and Rubocop

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Book < ApplicationRecord
-	validates :title, :author, :price, :published_date, presence: true
+  validates :title, :author, :price, :published_date, presence: true
 end
