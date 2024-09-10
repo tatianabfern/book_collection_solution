116c2bd96016cc2005ef653f1cef883bdaed5f46
Tatiana Fern <112017942+tatianabfern@users.noreply.github.com>
2024-09-09 22:58:43 -0500

    Book Collection 4 Solution

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: 'Tommy')
+User.create(username: 'Tatiana')
+
+Book.create(title: 'Magic Tree House', author: 'Mary Pope Osborne', price: 12.99, published_date: '1992-07-28')
+Book.create(title: 'The Vampire Diaries', author: 'L.J. Smith', price: 10.99, published_date: '1991-09-10')
